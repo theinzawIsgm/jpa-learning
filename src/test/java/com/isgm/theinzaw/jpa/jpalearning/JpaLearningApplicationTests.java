d197724aff841df4b3919722112449169bf516db
theinzaw <tzawmyanmar@gmail.com>
2020-02-25 23:16:50 +0630

    Initial Commit

---
+++ src/test/java/com/isgm/theinzaw/jpa/jpalearning/JpaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isgm.theinzaw.jpa.jpalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
